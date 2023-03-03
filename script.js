d92067f6bac660e1462c9418130de650b771c451
Robert Allred <rallred84@gmail.com>
2023-03-02 21:18:42 -0600

    Added js functions to change colors when tds are pressed, as well as options to change colors

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Your code here
-const table = document.querySelector('table');
+const table = document.querySelector("table");
 // let tr = document.createElement('tr');
 // let td = document.createElement('td');
 let rowNumber = 20;
-const addBtn = document.querySelector('#add-row');
+const addBtn = document.querySelector("#add-row");
+let tr;
 
 function addTd(num) {
-  let tr = document.createElement('tr');
+  tr = document.createElement("tr");
   while (num > 0) {
-    let td = document.createElement('td');
+    let td = document.createElement("td");
     tr.appendChild(td);
     // console.log(tr);
     num--;
@@ -18,9 +19,26 @@ function addTd(num) {
 
 function makeRow() {
   table.appendChild(addTd(rowNumber));
+
   //   addTd(rowNumber);
 }
 
-addBtn.addEventListener('click', makeRow);
+addBtn.addEventListener("click", makeRow);
 makeRow();
 makeRow();
+
+function colorize(e) {
+  e.target.classList.toggle(color);
+}
+
+table.addEventListener("click", colorize);
+
+let selectColor = document.querySelector("select");
+let color = "red";
+
+function colorChoose(e) {
+  color = e.target.value;
+  console.log(color);
+}
+
+selectColor.addEventListener("change", colorChoose);
